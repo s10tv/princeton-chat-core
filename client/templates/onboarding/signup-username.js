2a2a9ead6b25ae83d9f067d8ef11361df41fbe2a
Nurym Kudaibergen <nurim98@gmail.com>
2016-01-21 03:49:52 +0600

    signup-username wip

--- client/templates/onboarding/signup-username.js
+++ client/templates/onboarding/signup-username.js
@@ -16,22 +16,49 @@ Template.signupUsername.helpers({
 Template.signupUsername.events({
   'submit': function(event, template) {
     event.preventDefault();
-    var email = template.$('[name=email]').val();
-    var password = template.$('[name=password]').val();
-    var confirm = template.$('[name=confirm]').val();
+    const firstName = template.$('[name=firstName]').val();
+    const lastName = template.$('[name=lastName]').val();
+    const username = template.$('[name=username]').val();
+    const classYear = $('#classYear').val();
+    const parsedClassType = () => {
+      switch($('#classType').val()) {
+        case 'Undergraduate':
+          return 'undergrad';
+        case 'Graduate':
+          return 'grad';
+        default:
+          return null;
+      }
+    };
 
     var errors = {};
 
-    if (! email) {
-      errors.email = 'Email required';
+    if (!firstName) {
+      errors.firstName = "C'mon, all Tigers are given a first name!";
     }
 
-    if (! password) {
-      errors.password = 'Password required';
+    if (!lastName) {
+      errors.password = "You forgot to tell us how do you get called in the army!";
     }
 
-    if (confirm !== password) {
-      errors.confirm = "Passwords don't match";
+    if (firstName && lastName && (!firstName.match(/^[a-zA-Z]+$/) || !lastName.match(/^[a-zA-Z]+$/))) {
+      errors.wrongName = "Names only consist from letters, right?";
+    }
+
+    if (!username) {
+      errors.username = "You forgot to claim your username.";
+    }
+
+    if (username && !username.match(/^[a-zA-Z0-9]+$/)) {
+      errors.wrongUsername = "Usernames should only consist from letters and numbers";
+    }
+
+    if (classYear == "Class Year") {
+      errors.classYear = "When did you get your marvelous graduate cap?"
+    }
+
+    if (parsedClassType() == null) {
+      errors.classType = "You forgot to choose a class type."
     }
 
     Session.set(ERRORS_KEY, errors);
