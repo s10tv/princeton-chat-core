33d6bbfd2b608f5b2c39464ce8958372c682c8e5
Qiming Fang <qf26@cornell.edu>
2016-01-23 12:46:08 -0800

    add sendbird

--- client/configs/context.js
+++ client/configs/context.js
@@ -3,13 +3,16 @@ import {Meteor} from 'meteor/meteor';
 import {FlowRouter} from 'meteor/kadira:flow-router';
 import {ReactiveDict} from 'meteor/reactive-dict';
 import {Tracker} from 'meteor/tracker';
+import {sendbird} from './sendbird';
 
 export function initContext() {
+
   return {
     Meteor,
     FlowRouter,
     Collections,
     LocalState: new ReactiveDict(),
-    Tracker
+    Tracker,
+    sendbird
   };
 }
