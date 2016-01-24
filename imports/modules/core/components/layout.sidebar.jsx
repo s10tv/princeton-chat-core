94dae7563fb0ce075dde65aceba3587bdba7497f
Tony Xiao <tonyx.ca@gmail.com>
2016-01-24 14:58:44 -0800

    Nicely formatted sidebar header

--- imports/modules/core/components/layout.sidebar.jsx
+++ imports/modules/core/components/layout.sidebar.jsx
@@ -8,19 +8,43 @@ import CardHeader from 'material-ui/lib/card/card-header'
 import CardMedia from 'material-ui/lib/card/card-media'
 import CardTitle from 'material-ui/lib/card/card-title'
 import Avatar from 'material-ui/lib/avatar'
+import FlatButton from 'material-ui/lib/flat-button'
+import EnhancedButton from 'material-ui/lib/enhanced-button'
+import Divider from 'material-ui/lib/divider'
+import {Block, Flex} from 'jsxstyle'
+
+import FontIcon from 'material-ui/lib/font-icon'
+
+const SidebarHeader = () => (
+  <ListItem id='sidebar-header' innerDivStyle={{
+      paddingTop: 8,
+      paddingRight: 8,
+      paddingBottom: 8,
+      paddingLeft: 8,
+    }}>
+    <Flex>
+      <Avatar src='http://lorempixel.com/200/200/people/' style={{
+          borderRadius: 5,
+          width: 60,
+          height: 60,
+        }} />
+      <Flex flexGrow={1} marginLeft={8} flexDirection='column' justifyContent='space-around'>
+        <h3>Princeton.Chat</h3>
+        <Flex alignItems='center'>
+          <span className='online-status' />
+          <span>@tonyx</span>
+          <FontIcon className='material-icons' style={{marginLeft: 'auto'}}>expand_more</FontIcon>
+        </Flex>
+      </Flex>
+    </Flex>
+  </ListItem>
+  
+)
 
 export default ({content = () => null }) => (
   <LeftNav open={true} >
-    <CardHeader
-      title="Princeton.Chat"
-      subtitle="@tonyx"
-      avatar={
-        <Avatar src='http://lorempixel.com/200/200/people/' style={{
-            borderRadius: 5
-          }} />
-      } />
-    
-    
+    <SidebarHeader />  
+    <Divider />
     <List>
       <ListItem>First Item</ListItem>
       <ListItem>Second Item</ListItem>
@@ -30,3 +54,29 @@ export default ({content = () => null }) => (
 // <header>
 //   
 // </header>
+// <div>
+//   <EnhancedButton linkButton={true}>
+//     <Avatar src='http://lorempixel.com/200/200/people/' style={{
+//         borderRadius: 5
+//       }} />
+//     Princeton.Chat
+//     @tonyx
+//   </EnhancedButton>
+// </div>
+// <FlatButton style={{width: '100%'}}>
+//   <Avatar src='http://lorempixel.com/200/200/people/' style={{
+//       borderRadius: 5
+//     }} />
+//   Princeton.Chat
+//   @tonyx
+// </FlatButton>
+// <Divider />
+
+// <CardHeader
+//   title="Princeton.Chat"
+//   subtitle="@tonyx"
+//   avatar={
+//     <Avatar src='http://lorempixel.com/200/200/people/' style={{
+//         borderRadius: 5
+//       }} />
+//   } />
