6be2833d3a8751098c01d10fe8de3265567ae82f
Tony Xiao <tonyx.ca@gmail.com>
2016-01-26 11:16:25 -0800

    Allow enter key in textfield to submit, but shift enter to create new line

--- imports/modules/core/components/message.jsx
+++ imports/modules/core/components/message.jsx
@@ -23,9 +23,30 @@ export const MessageGroup = (props) => (
   </Flex>
 )
 
-export const InputBox = () => (
-  <Flex className='input-box' margin='0 16px 16px 16px' padding='0px 12px' border='2px solid #d9d9d9' borderRadius={5}>
-    <TextField multiLine={true} fullWidth={true} underlineShow={false} rowsMax={8}
-      hintText='Say something Im giving up on you' />
-  </Flex>
-)
+export class InputBox extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = {text: ''}
+  }
+  handleEnterKeyDown(event) {
+    if (!event.shiftKey) {
+      event.preventDefault()
+      console.log(`Will send message ${this.state.text}`)
+      this.setState({text: ''})
+    }
+  }
+  handleChange(event) {
+    this.setState({text: event.target.value})
+  } 
+  render() {
+    return (
+      <Flex className='input-box' margin='0 16px 16px 16px' padding='0px 12px' border='2px solid #d9d9d9' borderRadius={5}>
+        <TextField multiLine={true} fullWidth={true} underlineShow={false} rowsMax={8}
+          hintText='Say something Im giving up on you' 
+          value={this.state.text}
+          onChange={this.handleChange.bind(this)}
+          onEnterKeyDown={this.handleEnterKeyDown.bind(this)} />
+      </Flex>
+    )
+  }
+}
