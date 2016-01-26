8b51369ba63820671a77538076aaa670ca5ef71a
Qiming Fang <qf26@cornell.edu>
2016-01-26 14:14:34 -0800

    add a login guard for users who are not in session to be directed to home screen for login

--- imports/configs/routes.jsx
+++ imports/configs/routes.jsx
@@ -8,7 +8,15 @@ import PostSingle from '../modules/core/containers/post.details.js'
 import DirectMessage from '../modules/core/components/directMessage.jsx'
 import TopicList from '../modules/core/components/topic.list.jsx'
 
+import Login from '../modules/core/components/login.jsx'
+
 import WebFontLoader from 'webfontloader';
+
+function requireLogin(context) {
+  if (!Meteor.user()) {
+    return FlowRouter.go('home');
+  }
+}
 
 export default function (injectDeps) {
   WebFontLoader.load({
@@ -19,10 +27,19 @@ export default function (injectDeps) {
 
   const LayoutMainCtx = injectDeps(LayoutMain);
 
+  FlowRouter.triggers.enter([requireLogin], {except: ["home"]});
+
   FlowRouter.subscriptions = function() {
     this.register('topicsToFollow', Meteor.subscribe('topicsToFollow'));
     this.register('userData', Meteor.subscribe('userData'));
   };
+
+  FlowRouter.route('/', {
+    name: 'home',
+    action() {
+      mount(Login);
+    }
+  });
 
   FlowRouter.route('/topics/:topicId', {
     name: 'postList',
@@ -75,7 +92,7 @@ export default function (injectDeps) {
       })
     }
   });
-  
+
   FlowRouter.route('/x-topics', {
     action() {
       mount(LayoutMainCtx, {
