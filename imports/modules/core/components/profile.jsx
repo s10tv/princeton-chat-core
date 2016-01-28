e877c1a42b61deaba1f3d76285755b8681964007
Qiming Fang <qf26@cornell.edu>
2016-01-28 11:53:24 -0800

    remove clutter from profile

--- imports/modules/core/components/profile.jsx
+++ imports/modules/core/components/profile.jsx
@@ -13,33 +13,13 @@ const UserDetails = (props) => (
         <img src={props.user.avatar.url} className='profile-avatar' />
         <h2>{props.user.displayName}</h2>
         <h3>@{props.user.username}</h3>
-        <p>I wish i was a little bit taller, wish i was a baller, wish i had a girl… also.</p>
       </Flex>
     </section>
     <section className='profile-info'>
-      <h3>Followed Topics</h3>
-      <ul>
-        <li>#economics</li>
-        <li>#consulting</li>
-        <li>#firefly</li>
-        <li>#random</li>
-      </ul>
       <div className='profile-info-table'>
         <div className="row">
-          <label>User #</label>
-          <span>3</span>
-        </div>
-        <div className="row">
-          <label>Email that is super long</label>
-          <span>tonyx.xiddda.h2222.sa@alumni.princeton.edu</span>
-        </div>
-        <div className="row">
-          <label>Last active</label>
-          <span>Yesterday</span>
-        </div>
-        <div className="row">
           <label>Email</label>
-          <span>tonyx.ca@alumni.princeton.edu</span>
+          <span>{props.user.emails[0].address}</span>
         </div>
       </div>
     </section>
