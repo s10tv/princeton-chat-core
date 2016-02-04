77b25237a81fd0da15434e54c9de75651df680ad
Qiming Fang <qf26@cornell.edu>
2016-02-03 23:36:43 -0800

    stringify ironmq contents

--- server/lib/IronMQ.js
+++ server/lib/IronMQ.js
@@ -18,26 +18,10 @@ class IronMQ {
   }
 
   send({ payload }) {
-    console.log('ironmq sending', this.queueName, payload );
-
     const queue = this.client.queue(this.queueName);
-
-
-    const info = new Future();
-    const infoComplete = info.resolver();
-    this.client.queues({}, infoComplete);
-    Future.wait(info);
-    console.log(info.get());
-
-    const f = new Future();
-    const fComplete = f.resolver();
-    queue.info(fComplete);
-    Future.wait(f);
-    console.log(f.get());
-
     const future = new Future()
     const onComplete = future.resolver();
-    queue.post(payload, onComplete);
+    queue.post(JSON.stringify(payload), onComplete);
     Future.wait(future);
     return future.get();
   }
