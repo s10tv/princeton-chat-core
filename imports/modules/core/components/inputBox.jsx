89bab7f483d505833908b963e589f03736a51ba0
Tony Xiao <tonyx.ca@gmail.com>
2016-02-04 16:09:32 -0800

    Display input controls when user focuses on the input box

--- imports/modules/core/components/inputBox.jsx
+++ imports/modules/core/components/inputBox.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Flex, Inline} from 'jsxstyle'
 import TextField from 'material-ui/lib/text-field'
 import Avatar from 'material-ui/lib/avatar'
+import RaisedButton from 'material-ui/lib/raised-button'
+import Checkbox from 'material-ui/lib/checkbox'
 
 export default class InputBox extends React.Component {
   constructor(props) {
@@ -9,6 +11,7 @@ export default class InputBox extends React.Component {
     this.state = {
       text: '', 
       followToggleMouseOver: false,
+      inputFocused: false,
     }
   }
   
@@ -18,6 +21,14 @@ export default class InputBox extends React.Component {
       this.props.create(this.state.text, this.props.postId);
       this.setState({text: ''})
     }
+  }
+  
+  handleInputBlur() {
+    this.setState({inputFocused: false})
+  }
+  
+  handleInputFocus() {
+    this.setState({inputFocused: true})
   }
 
   handleChange(event) {
@@ -37,37 +48,63 @@ export default class InputBox extends React.Component {
     this.setState({followToggleMouseOver: false})
   }
   
+  renderFollowerControls() {
+    return (
+      <Flex alignItems='center' marginTop='12px' height={30}>
+        <Inline component='span' marginRight='8px'>
+          { this.props.followers.length > 0
+            ? (this.props.followers.length > 1 ? 'Followers:' : 'Follower:')
+            : 'No followers'
+          }
+        </Inline>
+        {this.props.followers.map(user =>
+          <Avatar key={user._id} style={{width: 30, height: 30}} src={user.avatar.url} />
+        )}
+        <Flex marginLeft='auto'>
+          <a style={{cursor: 'pointer'}}
+            onClick={this.handleFollowToggleClick.bind(this)}
+            onMouseOver={this.handleFollowToggleMouseOver.bind(this)}
+            onMouseOut={this.handleFollowToggleMouseOut.bind(this)} >
+            { this.props.isFollowing 
+              ? (this.state.followToggleMouseOver ? 'Unfollow' : 'Following')
+              : 'Follow' }
+          </a>
+        </Flex>
+      </Flex>
+    )
+  }
+  
+  renderInputControls() {
+    return (
+      <Flex alignItems='center' marginTop='12px' height={30}>
+        {this.props.followers.map(user =>
+          <Avatar key={user._id} style={{width: 30, height: 30}} src={user.avatar.url} />
+        )}
+        <Inline component='span' marginLeft='8px' marginRight='auto'>
+          { this.props.followers.length > 0
+            ? 'will be notified'
+            : 'No one will be notified' }
+        </Inline>
+        <Checkbox label='Press enter to send' style={{maxWidth: 210}} />
+        <RaisedButton label='Send' primary={true} />
+      </Flex>
+    )
+  }
+  
   render() {
+    const showInputControls = this.state.inputFocused || this.state.text.length > 0
     return (
       <Flex component='footer' flexDirection='column' padding='0 16px 8px 16px'>
         <Flex className='input-box' padding='0px 12px' border='2px solid #d9d9d9' borderRadius={5}>
           <TextField multiLine={true} fullWidth={true} underlineShow={false} rowsMax={8}
             hintText='Type a message...'
             value={this.state.text}
-            onChange={this.handleChange}
-            onEnterKeyDown={this.handleEnterKeyDown} />
+            onChange={this.handleChange.bind(this)}
+            onBlur={this.handleInputBlur.bind(this)}
+            onFocus={this.handleInputFocus.bind(this)}
+            onEnterKeyDown={this.handleEnterKeyDown.bind(this)} />
         </Flex>
-        <Flex alignItems='center' marginTop='12px' height={30}>
-          <Inline component='span' marginRight='8px'>
-            { this.props.followers.length > 0
-              ? (this.props.followers.length > 1 ? 'Followers:' : 'Follower:')
-              : 'No followers'
-            }
-          </Inline>
-          {this.props.followers.map(user =>
-            <Avatar key={user._id} style={{width: 30, height: 30}} src={user.avatar.url} />
-          )}
-          <Flex marginLeft='auto'>
-            <a style={{cursor: 'pointer'}}
-              onClick={this.handleFollowToggleClick.bind(this)}
-              onMouseOver={this.handleFollowToggleMouseOver.bind(this)}
-              onMouseOut={this.handleFollowToggleMouseOut.bind(this)} >
-              { this.props.isFollowing 
-                ? (this.state.followToggleMouseOver ? 'Unfollow' : 'Following')
-                : 'Follow' }
-            </a>
-          </Flex>
-        </Flex>
+        { showInputControls ? this.renderInputControls() : this.renderFollowerControls() }
       </Flex>
     )
   }
