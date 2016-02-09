c2c832d78b2aa75de476a373fbdc546fb3f55dcc
Tony Xiao <tonyx.ca@gmail.com>
2016-02-08 16:42:33 -0800

    Removing unused helper in topics

--- imports/configs/collections/topic.js
+++ imports/configs/collections/topic.js
@@ -2,18 +2,6 @@ import FollowerSchema from './common/follower';
 
 Topics = new Mongo.Collection('topics');
 
-// Calculate a default name for a list in the form of 'List A'
-Topics.defaultName = function() {
-  var nextLetter = 'A', nextName = 'List ' + nextLetter;
-  while (Lists.findOne({name: nextName})) {
-    // not going to be too smart here, can go past Z
-    nextLetter = String.fromCharCode(nextLetter.charCodeAt(0) + 1);
-    nextName = 'List ' + nextLetter;
-  }
-
-  return nextName;
-};
-
 TopicSchema = new SimpleSchema({
   displayName: { type: String },
   description: { type: String, optional: true },
