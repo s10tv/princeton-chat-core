d49fc9a108d590d1c0b8864fe6d7b0cc87ba00b6
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-09 23:15:59 -0800

    profile.jsx linted

--- imports/modules/core/components/profile.jsx
+++ imports/modules/core/components/profile.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import {Flex, Block} from 'jsxstyle'
+import { Flex } from 'jsxstyle'
 import Paper from 'material-ui/lib/paper'
-import Card from 'material-ui/lib/card/card'
-import Dialog from 'material-ui/lib/dialog';
+import Dialog from 'material-ui/lib/dialog'
 import { i18n } from '/imports/libs/mantra'
-import Avatar from 'material-ui/lib/avatar';
-import {LetterAvatar} from '/imports/modules/core/components/helpers.jsx'
+import Avatar from 'material-ui/lib/avatar'
+import { LetterAvatar } from '/imports/modules/core/components/helpers.jsx'
 
-const theme = i18n('primaryMuiTheme');
-const accent1Color = theme.baseTheme.palette.accent1Color;
+const theme = i18n('primaryMuiTheme')
+const accent1Color = theme.baseTheme.palette.accent1Color
 
 export default React.createClass({
   propTypes: {
@@ -25,40 +24,37 @@ export default React.createClass({
     /**
      * The user for whom the profile resembles.
      */
-    user: React.PropTypes.object,
+    user: React.PropTypes.object
   },
 
-  render() {
+  render () {
     // User must be in session in order to display a profile.
     if (!this.props.user) {
-      return null;
+      return null
     }
 
     return (
-      <Dialog
-        open={this.props.isOpen && this.props.user != undefined}
-        modal={false}
+      <Dialog open={this.props.isOpen && this.props.user !== undefined} modal={false}
         onRequestClose={this.props.handleClose}>
         <Paper>
           <section className='profile-header'>
-            <Flex flexDirection='column' alignItems='center' justifyContent='center' position='relative'
+            <Flex
+              flexDirection='column'
+              alignItems='center'
+              justifyContent='center'
+              position='relative'
               padding='36px'>
-              { this.props.user.avatar.isDefaultAvatar ?
-                <LetterAvatar
-                  size={150}
-                  color='white'
-                  backgroundColor={this.props.user.avatar.color}
-                >
-                  {this.props.user.avatarInitials}
-                </LetterAvatar>
-                :
-                <Avatar
-                  size={150}
-                  src={this.props.user.avatar.url} />
-              }
+              { this.props.user.avatar.isDefaultAvatar
+                ? <LetterAvatar size={150} color='white'
+                    backgroundColor={this.props.user.avatar.color}>
+                   {this.props.user.avatarInitials}
+                  </LetterAvatar>
+                : <Avatar size={150} src={this.props.user.avatar.url} />}
               <h1>{this.props.user.displayName}</h1>
               <h3 style={{ color: accent1Color }}>@{this.props.user.username}</h3>
-              <p>{this.props.user.emails[0].address}</p>
+              <p>
+                {this.props.user.emails[0].address}
+              </p>
             </Flex>
           </section>
         </Paper>
