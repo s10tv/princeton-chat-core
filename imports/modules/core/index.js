927ae5e5dbac242eeabbbb933910efdedc389ddd
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-10 00:56:26 -0800

    linted l/m/c index

--- imports/modules/core/index.js
+++ imports/modules/core/index.js
@@ -1,9 +1,9 @@
-import methodStubs from './configs/method_stubs';
-import actions from './actions';
+import methodStubs from './configs/method_stubs'
+import actions from './actions'
 
 export default {
   actions,
-  load(context) {
-    methodStubs(context);
+  load (context) {
+    methodStubs(context)
   }
-};
+}
