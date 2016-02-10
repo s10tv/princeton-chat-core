f4e7b59d54ce887ff2ff3fc05ab060216689a727
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-10 00:47:04 -0800

    layout.jsx linted

--- imports/modules/core/components/layout/layout.jsx
+++ imports/modules/core/components/layout/layout.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 import {StyleResizable} from 'material-ui/lib/mixins'
 import CreatePost from '/imports/modules/core/containers/post.create.js'
 import LogoutModal from '/imports/modules/core/containers/logout.modal.js'
-import Sidebar from '/imports/modules/core/containers/layout.sidebar.js';
+import Sidebar from '/imports/modules/core/containers/layout.sidebar.js'
 import Profile from '/imports/modules/core/containers/profile.js'
-import EditProfileModal from '/imports/modules/core/containers/editprofile.modal.js';
+import EditProfileModal from '/imports/modules/core/containers/editprofile.modal.js'
 import PostFollowersModal from '/imports/modules/core/containers/post.followers.modal.js'
-import GlobalSnackbar from '/imports/modules/core/containers/global.snackbar.js';
+import GlobalSnackbar from '/imports/modules/core/containers/global.snackbar.js'
 import { i18n } from '/imports/libs/mantra'
-
 
 // TODO: Figure out a better way that does not involve using Mixin
 
@@ -18,21 +17,27 @@ export default React.createClass({
   mixins: [
     StyleResizable
   ],
+
   childContextTypes: {
-    muiTheme: React.PropTypes.object,
+    muiTheme: React.PropTypes.object
   },
-  getChildContext() {
+
+  getChildContext () {
     return {
-      muiTheme: primaryMuiTheme,
+      muiTheme: primaryMuiTheme
     }
   },
-  render() {
-    const isAtLeastDesktop = this.isDeviceSize(StyleResizable.statics.Sizes.LARGE);
-    const isAtLastTablet = this.isDeviceSize(StyleResizable.statics.Sizes.MEDIUM);
-    const isAtLeastMobile = this.isDeviceSize(StyleResizable.statics.Sizes.SMALL);
 
-    const sidebarOpen = isAtLastTablet || isAtLeastDesktop;
-    const content = this.props.content || () => {};
+  propTypes: {
+    content: React.PropTypes.func.isRequired
+  },
+
+  render () {
+    const isAtLeastDesktop = this.isDeviceSize(StyleResizable.statics.Sizes.LARGE)
+    const isAtLastTablet = this.isDeviceSize(StyleResizable.statics.Sizes.MEDIUM)
+
+    const sidebarOpen = isAtLastTablet || isAtLeastDesktop
+    const content = this.props.content || (() => {})
 
     return (
       <div id='layout'>
