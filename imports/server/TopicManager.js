5a3bf218ed0b40311d977ddf0a08b51d7cce24f7
Qiming Fang <qf26@cornell.edu>
2016-02-09 21:27:10 -0800

    lint topicmanager

--- imports/server/TopicManager.js
+++ imports/server/TopicManager.js
@@ -1,29 +1,30 @@
-import { Topics, Users} from '/imports/configs/collections';
+import { check } from 'meteor/check'
+import { Topics, Users } from '/imports/configs/collections'
 
 export default class TopicManager {
-  static follow({ topicId, user }) {
-    check(topicId, String);
-    check(user, Object);
+  static follow ({ topicId, user }) {
+    check(topicId, String)
+    check(user, Object)
 
     Users.update(user._id, { $addToSet: {
-      followingTopics: topicId,
-    }});
+      followingTopics: topicId
+    }})
 
-    const topic = Topics.findOne(topicId);
-    if (topic.followers.filter(follower => follower.userId == user._id).length == 0) {
+    const topic = Topics.findOne(topicId)
+    if (topic.followers.filter(follower => follower.userId === user._id).length === 0) {
       Topics.update(topicId, { $addToSet: {
         followers: { userId: user._id, unreadCount: 0 }
-      }});
+      }})
     }
   }
 
-  static unfollow({ topicId, user }) {
+  static unfollow ({ topicId, user }) {
     Users.update(user._id, { $pull: {
-      followingTopics: topicId,
-    }});
+      followingTopics: topicId
+    }})
 
     Topics.update(topicId, { $pull: {
       followers: { userId: user._id }
-    }});
+    }})
   }
 }
