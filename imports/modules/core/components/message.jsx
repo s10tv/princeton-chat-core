9be9cce2b3bc68fd07ec4f0fae9cdf5ac657db67
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-10 00:06:08 -0800

    message.jsx linted

--- imports/modules/core/components/message.jsx
+++ imports/modules/core/components/message.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Flex, Block} from 'jsxstyle'
+import {Block} from 'jsxstyle'
 import IconMenu from 'material-ui/lib/menus/icon-menu'
 import MenuItem from 'material-ui/lib/menus/menu-item'
 import IconButton from 'material-ui/lib/icon-button'
@@ -8,7 +8,7 @@ import { i18n } from '/imports/libs/mantra'
 import {LetterAvatar} from '/imports/modules/core/components/helpers.jsx'
 import Avatar from 'material-ui/lib/avatar'
 
-const theme = i18n('secondaryMuiTheme');
+const theme = i18n('secondaryMuiTheme')
 
 export const Message = (props) => (
   <div className='message'>
@@ -18,11 +18,11 @@ export const Message = (props) => (
 )
 
 export class MessageGroup extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {hover: false, menuOpen: false}
   }
-  render() {
+  render () {
     const props = this.props
     return (
       <div className='message-group'
@@ -31,41 +31,43 @@ export class MessageGroup extends React.Component {
         style={{
           position: 'relative',
           display: 'flex',
-          padding: '8px 16px',
+          padding: '8px 16px'
         }}>
-        {(this.state.hover || this.state.menuOpen) && props.deleteMessage ?
-          <IconMenu
-            style={{position: 'absolute', top: 8, right: 8,}}
-            open={this.state.menuOpen}
-            onRequestChange={(open) => { this.setState({menuOpen: open}) }}
-            iconButtonElement={<IconButton><MoreHorizIcon /></IconButton>}
-            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}>
-            <MenuItem style={{color: 'red'}} primaryText="Delete message" onTouchTap={this.props.deleteMessage} />
-          </IconMenu> : null
+        { (this.state.hover || this.state.menuOpen) && props.deleteMessage
+          ? <IconMenu
+              style={{position: 'absolute', top: 8, right: 8}}
+              open={this.state.menuOpen}
+              onRequestChange={(open) => { this.setState({menuOpen: open}) }}
+              iconButtonElement={<IconButton><MoreHorizIcon /></IconButton>}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}>
+              <MenuItem style={{color: 'red'}} primaryText='Delete message'
+                onTouchTap={this.props.deleteMessage} />
+            </IconMenu>
+          : null
         }
-        <a href="#" onClick={props.showUserProfile}>
-          { props.owner.avatar.isDefaultAvatar ?
-            <LetterAvatar
-              size={50}
-              color='white'
-              backgroundColor={props.owner.avatar.color}
-            >{props.owner.avatarInitials}</LetterAvatar>
-            :
-            <Avatar
-              size={50}
-              src={props.owner.avatar.url} />
-            }
+        <a href='#' onClick={props.showUserProfile}>
+          { props.owner.avatar.isDefaultAvatar
+            ? <LetterAvatar
+                size={50}
+                color='white'
+                backgroundColor={props.owner.avatar.color}>
+                {props.owner.avatarInitials}
+              </LetterAvatar>
+            : <Avatar size={50} src={props.owner.avatar.url} />
+          }
         </a>
         <Block flex={1} marginLeft={8}>
           <header>
-            <a href="#" onClick={props.showUserProfile}>
+            <a href='#' onClick={props.showUserProfile}>
               <span className='display-name'>{ props.owner.displayName }</span>
             </a>
-            <a href="#" onClick={props.showUserProfile}>
+            <a href='#' onClick={props.showUserProfile}>
               <span className='mention' style={Object.assign({}, {
-                  color: theme.baseTheme.palette.accent1Color
-                })}>{ props.owner.displayUsername }</span>
+                color: theme.baseTheme.palette.accent1Color
+              })}>
+                { props.owner.displayUsername }
+              </span>
             </a>
             <span className='datetime'>{ props.timestamp }</span>
           </header>
@@ -77,5 +79,5 @@ export class MessageGroup extends React.Component {
 }
 
 MessageGroup.propTypes = {
-  deleteMessage: React.PropTypes.func,
+  deleteMessage: React.PropTypes.func
 }
