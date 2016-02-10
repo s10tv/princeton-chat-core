ca6894421a20d48ad2119efd69d75f9197ef4a01
Qiming Fang <qf26@cornell.edu>
2016-02-10 00:27:24 -0800

    lint onboarding

--- imports/modules/core/components/onboarding/setPassword.jsx
+++ imports/modules/core/components/onboarding/setPassword.jsx
@@ -1,67 +1,67 @@
-import React from 'react';
+import React from 'react'
 import TextField from 'material-ui/lib/text-field'
 import FlatButton from 'material-ui/lib/flat-button'
-import styles from '/imports/modules/core/components/styles.jsx';
+import styles from '/imports/modules/core/components/styles.jsx'
 
 export default React.createClass({
 
   propTypes: {
     addPassword: React.PropTypes.func.isRequired,
     shouldShowPasswordFields: React.PropTypes.bool.isRequired,
-    clickFacebook: React.PropTypes.func.isRequired,
+    clickFacebook: React.PropTypes.func.isRequired
   },
 
-  getInitialState() {
+  getInitialState () {
     return {
-      passwordText: '',
+      passwordText: ''
     }
   },
 
-  handleChange(event) {
+  handleChange (event) {
     this.setState({passwordText: event.target.value})
   },
 
-  setPassword() {
-    const password = this.refs.passwordField.getValue();
-    this.props.addPassword(password);
+  setPassword () {
+    const password = this.refs.passwordField.getValue()
+    this.props.addPassword(password)
   },
 
-  render() {
-    const { addPassword, shouldShowPasswordFields, clickFacebook } = this.props;
+  render () {
+    const { shouldShowPasswordFields, clickFacebook } = this.props
 
     return (
-      <div className="choose-password-section">
-        <div className="password-chooser">
+      <div className='choose-password-section'>
+        <div className='password-chooser'>
           <TextField
             ref='passwordField'
-            type="password"
+            type='password'
             floatingLabelText='Choose Password'
             onChange={this.handleChange}
             disabled={!shouldShowPasswordFields}
             onEnterKeyDown={this.setPassword} />
         </div>
 
-        { this.state.passwordText.length == 0 ? null : (
-          <div className="password-chooser">
-          <FlatButton
-            label="Set Password"
-            style={shouldShowPasswordFields ? styles.primaryButton : styles.disabledButton}
-            disabled={!shouldShowPasswordFields}
-            onClick={this.setPassword} />
+        {this.state.passwordText.length === 0 ? null : (
+          <div className='password-chooser'>
+            <FlatButton
+              label='Set Password'
+              style={shouldShowPasswordFields ? styles.primaryButton : styles.disabledButton}
+              disabled={!shouldShowPasswordFields}
+              onClick={this.setPassword} />
           </div>
         )}
 
-        { this.state.passwordText.length > 0 ? null : (
-          <div className="password-chooser" style={{ padding: "0px 8px"}}>
+        {this.state.passwordText.length > 0 ? null : (
+          <div className='password-chooser' style={{padding: '0px 8px'}}>
             OR
           </div>
         )}
 
-        { this.state.passwordText.length > 0 ? null : (
-          <div className="password-chooser">
+        {this.state.passwordText.length > 0 ? null : (
+          <div className='password-chooser'>
             <FlatButton
-              label="Link Facebook"
-              style={ shouldShowPasswordFields ? styles.facebookButton : styles.disabledButton }
+              label='Link Facebook'
+              style={shouldShowPasswordFields ? styles.facebookButton : styles.disabledButton}
               rippleColor='white'
               disabled={!shouldShowPasswordFields}
               onClick={clickFacebook} />
