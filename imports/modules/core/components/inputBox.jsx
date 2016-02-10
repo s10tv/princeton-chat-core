a4fdc688f0d81226684519ea9513d49506efdf44
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-09 23:27:11 -0800

    inputBox.jsx linted

--- imports/modules/core/components/inputBox.jsx
+++ imports/modules/core/components/inputBox.jsx
@@ -1,127 +1,153 @@
 import React from 'react'
-import {Flex, Inline} from 'jsxstyle'
+import { Flex, Inline } from 'jsxstyle'
 import TextField from 'material-ui/lib/text-field'
 import Avatar from 'material-ui/lib/avatar'
 import RaisedButton from 'material-ui/lib/raised-button'
 import Checkbox from 'material-ui/lib/checkbox'
 import linkState from 'react-link-state'
-import {LetterAvatar} from '/imports/modules/core/components/helpers.jsx'
+import { LetterAvatar } from '/imports/modules/core/components/helpers.jsx'
 
-export default class InputBox extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+export default React.createClass({
+  propTypes: {
+    /**
+     * The post which this message will be created for.
+     */
+    post: React.PropTypes.object.isRequired,
+    postId: React.PropTypes.string.isRequired,
+    followers: React.PropTypes.array.isRequired,
+    isFollowing: React.PropTypes.bool.isRequired,
+    follow: React.PropTypes.func.isRequired,
+    unfollow: React.PropTypes.func.isRequired,
+    showSnackbarError: React.PropTypes.func.isRequired,
+    showPostFollowers: React.PropTypes.func.isRequired,
+    /**
+     * The function called to create a message.
+     */
+    create: React.PropTypes.func.isRequired
+  },
+
+  getInitialState () {
+    return {
       text: '',
       followToggleMouseOver: false,
       inputFocused: false,
-      pressEnterToSend: true,
+      pressEnterToSend: true
     }
-  }
+  },
 
-  sendMessage() {
+  sendMessage () {
     this.props.create(this.state.text, this.props.postId)
     this.setState({text: ''})
-  }
+  },
 
-  handleEnterKeyDown(event) {
+  handleEnterKeyDown (event) {
     if (!event.shiftKey && this.state.pressEnterToSend) {
       event.preventDefault()
 
-      if (this.state.text.length == 0) {
+      if (this.state.text.length === 0) {
         return this.props.showSnackbarError('To add a newline, press [Shift] with [Enter]')
       }
 
-      if (this.state.text.trim().length == 0) {
+      if (this.state.text.trim().length === 0) {
         return this.props.showSnackbarError('Until everyone can communicate through whitespace, please input some text :)')
       }
 
       this.sendMessage()
     }
-  }
+  },
 
-  handleInputBlur() {
+  handleInputBlur () {
     this.setState({inputFocused: false})
-  }
+  },
 
-  handleInputFocus() {
+  handleInputFocus () {
     this.setState({inputFocused: true})
-  }
+  },
 
-  handleFollowToggleClick(e) {
+  handleFollowToggleClick (e) {
     e.preventDefault()
     this.props.isFollowing ? this.props.unfollow() : this.props.follow()
-  }
+  },
 
-  handleFollowToggleMouseOver() {
+  handleFollowToggleMouseOver () {
     this.setState({followToggleMouseOver: true})
-  }
+  },
 
-  handleFollowToggleMouseOut() {
+  handleFollowToggleMouseOut () {
     this.setState({followToggleMouseOver: false})
-  }
+  },
 
-  renderFollowerControls() {
+  renderFollowerControls () {
     return (
-      <Flex alignItems='center' marginTop='12px' height={30}>
-          <a href='#'
-            onClick={() => this.props.showPostFollowers(this.props.post.followers)}
-            style={{display: 'flex', alignItems: 'center'}}>
-            <Inline component='span' marginRight='8px'>
-              { this.props.followers.length > 0
-                ? (this.props.followers.length > 1 ? 'Followers:' : 'Follower:')
-                : 'No followers'
-              }
-            </Inline>
-            {this.props.followers.map(user =>
-              user.avatar.isDefaultAvatar ?
-              <LetterAvatar key={user._id} style={{marginRight: 3}} size={30} color='white'
-                backgroundColor={user.avatar.color}>{user.avatarInitials}</LetterAvatar>
-              :
-              <Avatar key={user._id} style={{marginRight: 3}} size={30} src={user.avatar.url} />
-            )}
-          </a>
-        <Flex marginLeft='auto'>
-          <a style={{cursor: 'pointer'}}
-            onClick={this.handleFollowToggleClick.bind(this)}
-            onMouseOver={this.handleFollowToggleMouseOver.bind(this)}
-            onMouseOut={this.handleFollowToggleMouseOut.bind(this)} >
-            { this.props.isFollowing
-              ? (this.state.followToggleMouseOver ? 'Unfollow' : 'Following')
-              : 'Follow' }
-          </a>
-        </Flex>
-      </Flex>
-    )
-  }
-
-  renderInputControls() {
-    return (
-      <Flex alignItems='center' marginTop='12px' height={30}>
-        {this.props.followers.map(user =>
-          user.avatar.isDefaultAvatar ?
-          <LetterAvatar key={user._id} size={30} style={{marginRight: 3}} color='white'
-            backgroundColor={user.avatar.color}>{user.avatarInitials}</LetterAvatar>
-          :
-          <Avatar key={user._id} style={{marginRight: 3}} size={30} src={user.avatar.url} />
+    <Flex alignItems='center' marginTop='12px' height={30}>
+      <a href='#' onClick={() => this.props.showPostFollowers(this.props.post.followers)}
+        style={{display: 'flex', alignItems: 'center'}}>
+        <Inline component='span' marginRight='8px'>
+          {this.props.followers.length > 0
+             ? (this.props.followers.length > 1 ? 'Followers:' : 'Follower:')
+             : 'No followers'} </Inline>
+        { this.props.followers.map(user => user.avatar.isDefaultAvatar
+          ? <LetterAvatar key={user._id} style={{marginRight: 3}} size={30} color='white'
+              backgroundColor={user.avatar.color}>
+              {user.avatarInitials}
+            </LetterAvatar>
+          : <Avatar key={user._id} style={{marginRight: 3}} size={30} src={user.avatar.url} />
         )}
-        <Inline component='span' marginLeft='8px' marginRight='auto'>
-          { this.props.followers.length > 0
-            ? 'will be notified'
-            : 'No one will be notified' }
-        </Inline>
-        <Checkbox checkedLink={linkState(this, 'pressEnterToSend')} label='Press enter to send' style={{maxWidth: 210}} />
-        { this.state.pressEnterToSend ? null
-          : <RaisedButton disabled={this.state.text.length == 0} label='Send' primary={true} onTouchTap={this.sendMessage.bind(this)} /> }
+      </a>
+      <Flex marginLeft='auto'>
+        <a
+          style={{cursor: 'pointer'}}
+          onClick={this.handleFollowToggleClick.bind(this)}
+          onMouseOver={this.handleFollowToggleMouseOver.bind(this)}
+          onMouseOut={this.handleFollowToggleMouseOut.bind(this)}>
+          {this.props.isFollowing
+             ? (this.state.followToggleMouseOver ? 'Unfollow' : 'Following')
+             : 'Follow'}
+        </a>
       </Flex>
+    </Flex>
     )
-  }
+  },
 
-  render() {
+  renderInputControls () {
+    return (
+    <Flex alignItems='center' marginTop='12px' height={30}>
+      {this.props.followers.map(user => user.avatar.isDefaultAvatar
+        ? <LetterAvatar key={user._id} size={30} style={{marginRight: 3}} color='white'
+            backgroundColor={user.avatar.color}>
+            {user.avatarInitials}
+          </LetterAvatar>
+        : <Avatar key={user._id} style={{marginRight: 3}} size={30} src={user.avatar.url} />
+      )}
+      <Inline component='span' marginLeft='8px' marginRight='auto'>
+        {this.props.followers.length > 0
+           ? 'will be notified'
+           : 'No one will be notified'}
+      </Inline>
+      <Checkbox checkedLink={linkState(this, 'pressEnterToSend')} label='Press enter to send'
+        style={{maxWidth: 210}} />
+      { this.state.pressEnterToSend
+        ? null
+        : <RaisedButton disabled={this.state.text.length === 0} label='Send' primary={true}
+            onTouchTap={this.sendMessage.bind(this)} />}
+    </Flex>
+    )
+  },
+
+  render () {
     const showInputControls = this.state.inputFocused || this.state.text.length > 0
     return (
       <Flex component='footer' flexDirection='column' padding='0 16px 8px 16px'>
-        <Flex className='input-box' padding='0px 12px' border='2px solid #d9d9d9' borderRadius={5}>
-          <TextField multiLine={true} fullWidth={true} underlineShow={false} rowsMax={8}
+        <Flex
+          className='input-box'
+          padding='0px 12px'
+          border='2px solid #d9d9d9'
+          borderRadius={5}>
+          <TextField
+            multiLine={true}
+            fullWidth={true}
+            underlineShow={false}
+            rowsMax={8}
             hintText='Type a message...'
             value={this.state.text}
             onChange={(e) => this.setState({text: e.target.value})}
@@ -129,25 +155,8 @@ export default class InputBox extends React.Component {
             onFocus={this.handleInputFocus.bind(this)}
             onEnterKeyDown={this.handleEnterKeyDown.bind(this)} />
         </Flex>
-        { showInputControls ? this.renderInputControls() : this.renderFollowerControls() }
+        {showInputControls ? this.renderInputControls() : this.renderFollowerControls()}
       </Flex>
     )
   }
-}
-
-InputBox.propTypes = {
-  /**
-   * The post which this message will be created for.
-   */
-  post: React.PropTypes.object.isRequired,
-  postId: React.PropTypes.string.isRequired,
-  followers: React.PropTypes.array.isRequired,
-  isFollowing: React.PropTypes.bool.isRequired,
-  follow: React.PropTypes.func.isRequired,
-  unfollow: React.PropTypes.func.isRequired,
-  showSnackbarError: React.PropTypes.func.isRequired,
-  /**
-   * The function called to create a message.
-   */
-  create: React.PropTypes.func.isRequired,
-}
+})
