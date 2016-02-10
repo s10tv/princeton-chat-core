96f2971229726552fd43ff90414b1ad8789f623f
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-10 00:31:07 -0800

    linted post followers modal

--- imports/modules/core/components/post.followers.modal.jsx
+++ imports/modules/core/components/post.followers.modal.jsx
@@ -1,47 +1,23 @@
-import React from 'react';
-import Toolbar from 'material-ui/lib/toolbar/toolbar';
-import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
-import ToolbarSeparator from 'material-ui/lib/toolbar/toolbar-separator';
-import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
-import List from 'material-ui/lib/lists/list';
-import ListItem from 'material-ui/lib/lists/list-item';
-import Dialog from 'material-ui/lib/dialog';
-import RaisedButton from 'material-ui/lib/raised-button';
-import Avatar from 'material-ui/lib/avatar';
-import Menu from '/imports/modules/core/components/menu.jsx';
+import React from 'react'
+import Toolbar from 'material-ui/lib/toolbar/toolbar'
+import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group'
+import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title'
+import List from 'material-ui/lib/lists/list'
+import ListItem from 'material-ui/lib/lists/list-item'
+import Dialog from 'material-ui/lib/dialog'
+import Avatar from 'material-ui/lib/avatar'
 import IconButton from 'material-ui/lib/icon-button'
 import FontIcon from 'material-ui/lib/font-icon'
-import {LetterAvatar} from '/imports/modules/core/components/helpers.jsx';
+import {LetterAvatar} from '/imports/modules/core/components/helpers.jsx'
 
 export default class PostFollowersModal extends React.Component {
-  propTypes: {
-    // /**
-    //  * Proptypes are also indirectly passed to Menu
-    //  */
-    // ...Menu.propTypes,
-
-    // Modal view closing handler
-    closeModal: React.PropTypes.func.isRequired,
-
-    // A list of followers to render
-    followers: React.PropTypes.array.isRequired,
-
-    // Show/Hide Modal view
-    isOpen: React.PropTypes.bool.isRequired,
-
-    /**
-     * Func to show user profile modal
-     */
-    showUserProfile: React.PropTypes.func.isRequired
-  }
-
-  render() {
+  render () {
     const {
       closeModal,
       followers,
       isOpen,
       showUserProfile
-    } = this.props;
+    } = this.props
 
     const toolbar =
       <Toolbar>
@@ -49,8 +25,8 @@ export default class PostFollowersModal extends React.Component {
           <ToolbarTitle text='Followers' />
         </ToolbarGroup>
         <ToolbarGroup float='right' style={{top: '50%', transform: 'translateY(-50%)'}}>
-          <IconButton tooltip="Close" onTouchTap={closeModal}>
-            <FontIcon className="material-icons">clear</FontIcon>
+          <IconButton tooltip='Close' onTouchTap={closeModal}>
+            <FontIcon className='material-icons'>clear</FontIcon>
           </IconButton>
         </ToolbarGroup>
       </Toolbar>
@@ -68,20 +44,36 @@ export default class PostFollowersModal extends React.Component {
               key={follower._id}
               onTouchTap={() => showUserProfile(follower)}
               primaryText={follower.displayEmail}
-              leftAvatar={
-                follower.avatar.isDefaultAvatar ?
-                <LetterAvatar
-                  color='white'
-                  backgroundColor={follower.avatar.color}
-                >
-                {follower.avatarInitials}
-                </LetterAvatar>
-                :
-                <Avatar src={follower.avatar.src} />
+              leftAvatar={ follower.avatar.isDefaultAvatar
+                ? <LetterAvatar color='white' backgroundColor={follower.avatar.color}>
+                    {follower.avatarInitials}
+                  </LetterAvatar>
+                : <Avatar src={follower.avatar.src} />
               }
-            /> )}
+            />
+          )}
         </List>
       </Dialog>
     )
   }
+}
+
+PostFollowersModal.propTypes = {
+  // /**
+  //  * Proptypes are also indirectly passed to Menu
+  //  */
+
+  // Modal view closing handler
+  closeModal: React.PropTypes.func.isRequired,
+
+  // A list of followers to render
+  followers: React.PropTypes.array.isRequired,
+
+  // Show/Hide Modal view
+  isOpen: React.PropTypes.bool.isRequired,
+
+  /**
+   * Func to show user profile modal
+   */
+  showUserProfile: React.PropTypes.func.isRequired
 }
