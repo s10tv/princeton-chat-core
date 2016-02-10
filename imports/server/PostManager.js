945ef8cd2c5c940de8643677e550fc6f493c5b91
Qiming Fang <qf26@cornell.edu>
2016-02-09 21:25:18 -0800

    lint postmanager

--- imports/server/PostManager.js
+++ imports/server/PostManager.js
@@ -1,57 +1,57 @@
-import { Topics, Posts, Users} from '/imports/configs/collections';
+import check from 'meteor/check'
+import { Topics, Posts, Users } from '/imports/configs/collections'
 
 export default class PostManager {
 
-  static follow({ user, postId }) {
-    check(user, Object);
-    check(postId, String);
+  static follow ({ user, postId }) {
+    check(user, Object)
+    check(postId, String)
 
     const post = Posts.findOne(postId)
 
     if (!post) {
-      return;
+      return
     }
 
     Users.update(user._id, { $addToSet: {
-      followingPosts: post._id,
-    }});
+      followingPosts: post._id
+    }})
 
-
-    if (post.followers.filter(follower => follower.userId == user._id).length == 0) {
+    if (post.followers.filter(follower => follower.userId === user._id).length === 0) {
       Posts.update(post._id, { $addToSet: {
         followers: { userId: user._id, unreadCount: 0 }
-      }});
+      }})
     }
 
     post.topicIds.forEach(topicId => {
       Topics.update(topicId, { $set: {
         numPosts: Posts.find({ isDM: { $ne: true }, topicIds: topicId }).count()
-      }});
-    });
+      }})
+    })
   }
 
-  static unfollow({ postId, user }) {
-    check(user, Object);
-    check(postId, String);
+  static unfollow ({ postId, user }) {
+    check(user, Object)
+    check(postId, String)
 
     const post = Posts.findOne(postId)
 
     if (!post) {
-      return;
+      return
     }
 
     Users.update(user._id, { $pull: {
-      followingPosts: post._id,
-    }});
+      followingPosts: post._id
+    }})
 
     Posts.update(post._id, { $pull: {
       followers: { userId: user._id }
-    }});
+    }})
 
     post.topicIds.forEach(topicId => {
       Topics.update(topicId, { $set: {
         numPosts: Posts.find({ isDM: { $ne: true }, topicIds: topicId }).count()
-      }});
-    });
+      }})
+    })
   }
 }
