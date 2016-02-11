5fefcd22546a4ffc88e192c2cda2a5cef7b31a35
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-11 14:42:33 -0800

    new layout of topic discovery

--- client/modules/core/components/topic.list.jsx
+++ client/modules/core/components/topic.list.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Flex, Block} from 'jsxstyle'
+import {Flex} from 'jsxstyle'
 import FlatButton from '../../../../node_modules/material-ui/lib/flat-button'
 import FontIcon from '../../../../node_modules/material-ui/lib/font-icon'
-import Menu from '/client/modules/core/components/menu.jsx'
 import styles from '/client/modules/core/components/styles.jsx'
+import { RaisedButton } from '/client/lib/ui'
 
 export default React.createClass({
   propTypes: {
@@ -15,18 +15,10 @@ export default React.createClass({
 
   render () {
     return (
-      <main style={Object.assign({}, styles.main, { marginLeft: this.props.sidebarOpen ? 240 : 0 })}>
-        <Menu {...this.props}
-          title={'Follow some topics'}
-          hideFollowerSection
-          hideFollowActionSection />
-
+      <main style={Object.assign({}, styles.main, { marginLeft: this.props.sidebarOpen ? 240 : 0,
+        overflowY: 'scroll' })}>
         <Flex flex={1} flexDirection='column' alignItems='center'>
-          <h1>Browse all topics</h1>
-          <p>
-            Check the ones you want to follow
-          </p>
-          <TopicGrid margin='0 36px 36px 36px' {...this.props} />
+          <TopicGrid margin={36} {...this.props} />
         </Flex>
       </main>
     )
@@ -59,7 +51,7 @@ export const TopicGrid = React.createClass({
   render () {
     return (
       <div>
-        <Flex maxWidth={1200} flexWrap='wrap' justifyContent='center' {...this.props}>
+        <Flex flexWrap='wrap' maxWidth={1000} justifyContent='center' {...this.props}>
           {this.props.topics.map((topic) =>
             <TopicListItem
               key={topic._id}
@@ -78,32 +70,55 @@ export const TopicGrid = React.createClass({
 })
 
 const TopicListItem = ({topic, followTopic, unfollowTopic}) => {
-  const topicPostsCountFollowersCountStr = () => {
-    var str = []
-    if (topic.numPosts) {
-      str.push(`${topic.numPosts} posts`)
+  const pluralizeTextForNumber = (text, number) => {
+    if (number !== 1) {
+      return text + 's'
     }
-    if (topic.followersCount) {
-      str.push(`${topic.followersCount} followers`)
-    }
-    return str.join(' - ')
+
+    return text
+  }
+
+  const getFollowerText = () => {
+    return pluralizeTextForNumber('follower', topic.followersCount)
+  }
+
+  const getPostsText = () => {
+    return pluralizeTextForNumber('post', topic.numPosts)
   }
 
   return (
-    <Flex width={300} height={80} backgroundColor='#F2F6F9' padding='16px 4px 16px 16px' margin={8} alignItems='center'>
-      <Block flex={1}>
-        <Block fontWeight={600}>#{topic.displayName}</Block>
-        <Block color='#8899A6' fontSize={14}>{topicPostsCountFollowersCountStr()}</Block>
-      </Block>
-
-      {
-        topic.isFollowed
-        ? <FlatButton primary
-          label='Following' onTouchTap={() => { unfollowTopic(topic._id) }} />
-        : <FlatButton label='Follow' labelPosition='after'
-          icon={<FontIcon className='material-icons'>add</FontIcon>}
-          onTouchTap={() => { followTopic(topic._id) }} />
-      }
+    <Flex width={400} flexDirection='column' margin={10}>
+      <Flex>
+        <Flex width={200} backgroundImage={`url("${topic.cover.url}")`}
+          backgroundSize='cover' backgroundPosition='center' borderRadius={5} />
+        <Flex flexDirection='column' marginLeft={30}>
+          <h3 style={{ fontWeight: 400, marginTop: 0, marginBottom: 0 }}>#{topic.displayName}</h3>
+          <Flex flexDirection='row' marginTop={15}>
+            {
+              topic.followersCount !== undefined
+              ? <Flex flexDirection='column' marginRight={25} alignItems='center'>
+                  <span style={{fontWeight: 300}}>{topic.followersCount}</span>
+                  <span style={{fontWeight: 300, color: '#c3c3c3'}}>{getFollowerText()}</span>
+                </Flex> : null
+            }
+            {
+              topic.numPosts !== undefined
+              ? <Flex flexDirection='column' alignItems='center'>
+                  <span style={{fontWeight: 300}}>{topic.numPosts}</span>
+                  <span style={{fontWeight: 300, color: '#c3c3c3'}}>{getPostsText()}</span>
+                </Flex> : null
+            }
+          </Flex>
+          {
+            topic.isFollowed
+            ? <RaisedButton secondary style={{marginTop: 15, width: 150}}
+              label='Following' onTouchTap={() => { unfollowTopic(topic._id) }} />
+            : <RaisedButton primary style={{marginTop: 15, width: 150}}
+              label='Follow' onTouchTap={() => { followTopic(topic._id) }} />
+          }
+        </Flex>
+      </Flex>
+      <p style={{marginTop: 15, fontWeight: 300}}>{topic.description}</p>
     </Flex>
   )
 }
