8c8935eacd5c1991e651761e043249d0beb3d18f
Qiming Fang <qf26@cornell.edu>
2016-02-11 14:10:00 -0800

    fix Users not defined error in server

--- server/guest-login.js
+++ server/guest-login.js
@@ -1,16 +1,18 @@
+import { Accounts } from 'meteor/accounts-base'
+import { Users } from '/lib/collections'
 import { isValidHash } from '/server/lib/Auth'
 
 Accounts.registerLoginHandler('guest', (serviceData) => {
   if (!serviceData.guest) {
-    return undefined;
+    return undefined
   }
 
-  const user = Users.findOne(serviceData.guest.userId);
+  const user = Users.findOne(serviceData.guest.userId)
   if (isValidHash(user, serviceData.guest.hash)) {
     return {
-      userId: user._id,
+      userId: user._id
     }
   }
 
-  return undefined;
-});
+  return undefined
+})
