58604c72a8d300bc72150f56ed4b5f8da8926179
Tony Xiao <tonyx.ca@gmail.com>
2016-02-13 11:24:30 -0800

    Removing extraneous file

---
+++ server/http/index.jsx
@@ -0,0 +1,58 @@
+import invariant from 'invariant'
+import ReactDOMServer from 'react-dom/server'
+import React from 'react'
+import {Posts, Users} from '/lib/collections'
+import {isValidHash} from '../lib/Auth'
+import PostManager from '../lib/PostManager'
+import GuestToggleFollow from './toggleFollowing.jsx'
+import htmlPage from './html'
+
+export default function () {
+  HTTP.methods({
+    'guest/posts/:postId/:action': function() {
+      const {postId, action} = this.params;
+      const {userId, hash} = this.query;
+
+      const user = Users.findOne(userId)
+      if (!user) {
+        this.setStatusCode(404)
+        return 'Not Found'
+      }
+
+      if (!isValidHash(user, hash)) {
+        this.setStatusCode(404)
+        return 'Not Found'
+      }
+
+      const post = Posts.findOne(postId)
+      if (!post) {
+        this.setStatusCode(404)
+        return 'Not Found'
+      }
+      let isFollowing = false
+
+      if (action === 'follow') {
+        PostManager.follow({ user, postId: postId })
+        isFollowing = true
+      } else if (action === 'unfollow') {
+        PostManager.unfollow({ user, postId: postId })
+        isFollowing = false
+      } else {
+        this.setStatusCode(404)
+        return 'Not Found - Action not allowed'
+      }
+      return htmlPage({
+        title: 'Princeton.Chat',
+        body: ReactDOMServer.renderToStaticMarkup(
+          <GuestToggleFollow
+            title={post.title}
+            isFollowing={isFollowing}
+            followLink={`/guest/posts/${postId}/follow?userId=${userId}&hash=${hash}`}
+            unfollowLink={`/guest/posts/${postId}/unfollow?userId=${userId}&hash=${hash}`}
+            editTopicsLink={`/guest?userId=${userId}&hash=${hash}`}
+          />
+        )
+      })
+    }
+  })
+}
--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
-import publications from './publications';
-import methods from './methods';
+import publications from './publications'
+import methods from './methods'
+import http from './http/index.jsx'
 //import addInitialData from './configs/initial_adds.js'; // LOAD FIXTURES
 
-publications();
-methods();
+publications()
+methods()
+http()
 //addInitialData();
