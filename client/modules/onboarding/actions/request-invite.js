275a46c4b76d15ef1d3852de92f6409fe9957df9
Tony Xiao <tonyx.ca@gmail.com>
2016-02-12 17:37:22 -0800

    handleSubmit already gives us the correct data, no need to validate / extract again

--- client/modules/onboarding/actions/request-invite.js
+++ client/modules/onboarding/actions/request-invite.js
@@ -1,32 +1,14 @@
-import { validator } from '/lib/validation/request-invite-validation'
-
 export default {
-  verifyAffiliation ({Meteor, store}) {
-    const { form: { affiliation: {
-      firstName, lastName, birthDate, classYear, degree, email
-    }}} = store.getState()
+  verifyAffiliation ({Meteor}, data) {
+    Meteor.call('signup/verifyAffiliation', data, (err) => {
+      if (err) {
+        // TODO(tonyx): Display Error
+        console.log(err)
+        return
+      }
 
-    const info = {
-      firstName: firstName.value,
-      lastName: lastName.value,
-      birthDate: birthDate.value,
-      classYear: classYear.value,
-      degree: degree.value,
-      email: email.value
-    }
-
-    const errors = validator(info)
-    if (Object.keys(errors).length === 0) {
-      Meteor.call('signup/verifyAffiliation', info, (err) => {
-        if (err) {
-          // TODO(tonyx): Display Error
-          console.log(err)
-          return
-        }
-
-        // TODO(tonyx): Display success indicator
-        console.log('inserted invite')
-      })
-    }
+      // TODO(tonyx): Display success indicator
+      console.log('inserted invite')
+    })
   }
 }
