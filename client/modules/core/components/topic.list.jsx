a8f059eba6e4f1874b85505979623e8398712a60
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-13 18:21:20 -0800

    [topic list] organized all the styles in a separate object

--- client/modules/core/components/topic.list.jsx
+++ client/modules/core/components/topic.list.jsx
@@ -56,14 +56,12 @@ export default React.createClass({
   },
 
   tabItemClicked (e) {
-    console.log(e.currentTarget.text)
     this.setState({
       value: e.currentTarget.text
     })
   },
 
   ifLoggedInExecute (func) {
-    console.log(func, this.props.isLoggedIn)
     return this.props.isLoggedIn ? func() : this.props.showSweetAlertToLogin()
   },
 
@@ -71,12 +69,8 @@ export default React.createClass({
     return (
       <main style={Object.assign({}, styles.main, { marginLeft: this.props.sidebarOpen ? 240 : 0,
         padding: '0px 40px', alignItems: 'center'})}>
-        <Flex flexGrow={1} flexDirection='column' alignItems='center' style={{
-          maxWidth: 600,
-          maxHeight: '100vh'
-        }}>
-          <Flex flex='1 0 auto' alignSelf='stretch'
-            marginBottom={10} boxShadow='#dedede 0px -1px 0px 0px inset'>
+        <Flex style={localStyle.mainFlexContainer}>
+          <Flex style={localStyle.tabFlexContainer}>
             <a onClick={this.tabItemClicked} href='#'>
               <h3 style={Object.assign({},
                   localStyle.tabItem,
@@ -157,40 +151,36 @@ const TopicListItem = ({isLoggedIn, ifLoggedInExecute,
   }
 
   return (
-    <Flex flexDirection='column' margin='25px 0px' flexGrow={1}>
+    <Flex style={localStyle.topicItemContainer}>
       <Flex flexGrow={1}>
         <a href='#' onClick={() => ifLoggedInExecute(() => {
           navigateToTopic(topic._id)
-        })} style={{
-          flex: '0 0 220px',
-          maxHeight: 167,
-          backgroundImage: `url("${topic.cover.url}")`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          borderRadius: 5
-        }} />
+        })} style={Object.assign({},
+          localStyle.topicItemCoverPhoto,
+          { backgroundImage: `url("${topic.cover.url}")` }
+        )} />
         <Flex flexGrow={1} flexDirection='column' marginLeft={30}>
-          <h3 style={{ fontWeight: 400, marginTop: 0, marginBottom: 0 }}>
+          <h3 style={localStyle.textTopicTitle}>
             <a href='#' onClick={() => ifLoggedInExecute(() => {
               navigateToTopic(topic._id)
             })}>
               #{topic.displayName}
             </a>
           </h3>
-          <h5 style={{fontWeight: 300, marginTop: 5, marginBottom: 0}}>
+          <h5 style={localStyle.textTopicEmail}>
             {topic._id}@{i18n('topicMailServer')}
           </h5>
           <Flex marginTop={15} alignItems='center'>
             {
               topic.followersCount !== undefined
               ? <Flex marginRight={25}>
-                <span style={{fontWeight: 300, color: '#c3c3c3'}}>{topic.followersCount} {getFollowerText()}</span>
+                <span style={localStyle.topicItemCountInfo}>{topic.followersCount} {getFollowerText()}</span>
               </Flex> : null
             }
             {
               topic.numPosts !== undefined
-              ? <Flex flexDirection='column' alignItems='center'>
-                <span style={{fontWeight: 300, color: '#c3c3c3'}}>{topic.numPosts} {getPostsText()}</span>
+              ? <Flex>
+                <span style={localStyle.topicItemCountInfo}>{topic.numPosts} {getPostsText()}</span>
               </Flex> : null
             }
           </Flex>
@@ -201,10 +191,7 @@ const TopicListItem = ({isLoggedIn, ifLoggedInExecute,
               icon={<FontIcon className='material-icons'>done</FontIcon>}
               backgroundColor={i18n('primaryMuiTheme').rawTheme.palette.primary1Color}
               hoverColor={i18n('primaryMuiTheme').rawTheme.palette.lightenedPrimary1Color}
-              style={{marginTop: 15,
-                color: 'white',
-                textTransform: 'inherit'
-              }}
+              style={localStyle.btnSubscribe}
               label='Subscribed'
               onTouchTap={() => ifLoggedInExecute(() => unfollowTopic(topic._id))} />
             : <FlatButton
@@ -212,16 +199,13 @@ const TopicListItem = ({isLoggedIn, ifLoggedInExecute,
               icon={<FontIcon className='material-icons'>add</FontIcon>}
               backgroundColor={i18n('primaryMuiTheme').rawTheme.palette.accent1Color}
               hoverColor={i18n('primaryMuiTheme').rawTheme.palette.lightenedAccent1Color}
-              style={{marginTop: 15,
-                color: 'white',
-                textTransform: 'inherit'
-              }}
+              style={localStyle.btnSubscribe}
               label='Subscribe'
               onTouchTap={() => ifLoggedInExecute(() => followTopic(topic._id))} />
           }
         </Flex>
       </Flex>
-      <p style={{marginTop: 20, fontWeight: 300, marginBottom: 0}}>{topic.description}</p>
+      <p style={localStyle.topicItemDescription}>{topic.description}</p>
     </Flex>
   )
 }
@@ -230,21 +214,26 @@ const NewTopicButton = ({ showAddTopicModal }) => {
   return (
     <a href='#' onClick={showAddTopicModal}>
       <Flex margin='25px 0px' flexGrow={1}>
-        <Flex flex='0 0 220px' maxHeight={147} backgroundColor='#e0e0e0'
-          borderRadius={5} justifyContent='center' alignItems='center'>
+        <Flex style={newTopicButtonStyle.coverPhotoBg}>
           <FontIcon className='material-icons' color='#757575'>photo_camera</FontIcon>
         </Flex>
         <Flex flexGrow={1} marginLeft={30} flexDirection='column' alignItems='center'>
-          <h3 style={{ fontWeight: 400, marginTop: 0, marginBottom: 0 }}>
+          <h3 style={localStyle.textTopicTitle}>
             Create a new channel
           </h3>
-          <p style={{marginTop: 15, fontWeight: 300}}>Can't find what you are looking for? No problem.
+          <p style={localStyle.topicItemDescription}>Can't find what you are looking for? No problem.
           Channels in {i18n('title')} are curated by the community. Create your own and we'll help
           you invite other people to follow your channel.</p>
         </Flex>
       </Flex>
     </a>
   )
+}
+
+const coverPhotoDimensions = {
+  flex: '0 0 220px',
+  maxHeight: 147,
+  borderRadius: 5
 }
 
 const localStyle = {
@@ -262,5 +251,70 @@ const localStyle = {
   tabItemDeselected: {
     borderBottom: '',
     color: '#dedede'
+  },
+
+  textTopicEmail: {
+    fontWeight: 300,
+    marginTop: 5,
+    marginBottom: 0
+  },
+
+  textTopicTitle: {
+    fontWeight: 400,
+    marginTop: 0,
+    marginBottom: 0
+  },
+
+  btnSubscribe: {
+    marginTop: 15,
+    color: 'white',
+    textTransform: 'inherit'
+  },
+
+  mainFlexContainer: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: 600,
+    maxHeight: '100vh'
+  },
+
+  tabFlexContainer: {
+    flex: '1 0 auto',
+    alignSelf: 'stretch',
+    marginBottom: 10,
+    boxShadow: '#dedede 0px -1px 0px 0px inset'
+  },
+
+  topicItemContainer: {
+    flexDirection: 'column',
+    margin: '25px 0px',
+    flexGrow: 1
+  },
+
+  topicItemCoverPhoto: {
+    ...coverPhotoDimensions,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+
+  topicItemCountInfo: {
+    fontWeight: 300,
+    color: '#c3c3c3'
+  },
+
+  topicItemDescription: {
+    marginTop: 20,
+    fontWeight: 300,
+    marginBottom: 0
+  }
+}
+
+const newTopicButtonStyle = {
+  coverPhotoBg: {
+    ...coverPhotoDimensions,
+    backgroundColor: '#e0e0e0',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 }
