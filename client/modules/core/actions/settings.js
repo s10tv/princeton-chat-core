29f09d72f6ff5e445d073489fd3c8dfeda14551d
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-14 16:31:04 -0800

    redirect to home page after logout

--- client/modules/core/actions/settings.js
+++ client/modules/core/actions/settings.js
@@ -2,7 +2,7 @@ export default {
   logout ({ LocalState, Meteor, FlowRouter }) {
     LocalState.set('SETTINGS_DIALOG_SHOWING', false) // first close this
     Meteor.logout()
-    FlowRouter.go('home')
+    FlowRouter.go('/')
   },
 
   showSettingsModal ({ LocalState }) {
