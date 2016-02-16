a15eb74cbf0d8a4b8f1df464e8ab14db86fd9b79
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-16 13:47:18 -0800

    added google analytics key to meteor settings

--- server/configs/settings.js
+++ server/configs/settings.js
@@ -20,6 +20,7 @@ export default function () {
   _.extend(Meteor.settings.public, {
     audience: process.env.AUDIENCE || 'princeton',
     amplitudeKey: process.env.AMPLITUDE_KEY || 'bc1101820f7bda64561e70be2594befd',
+    googleAnalyticsKey: process.env.GOOGLE_ANALYTICS_KEY || 'UA-73874104-1',
     unsplashKey: process.env.UNSPLASH_KEY || '3b28c5415d07e18260e66e9d5bc3f2434c2aa64bed071d52e8c62c3da800612e'
   })
 }
