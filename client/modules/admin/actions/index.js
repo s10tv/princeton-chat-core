65bd481edeb81d39f3fa602dec1ce42e48ba475a
Qiming Fang <qf26@cornell.edu>
2016-02-17 14:03:54 -0800

    alert before inviting/deleting users https://app.asana.com/0/77471202261980/91020443084767 https://app.asana.com/0/77471202261980/91308433288162

--- client/modules/admin/actions/index.js
+++ client/modules/admin/actions/index.js
@@ -1,12 +1,28 @@
 export default {
   adminInvite: {
-    sendInvite ({Meteor, sweetalert}, inviteId) {
-      Meteor.call('admin/user/invite', inviteId, (err) => {
-        if (err) {
-          return sweetalert({title: 'Error inviting', text: err.reason})
-        }
-        return sweetalert({title: 'Invited', text: 'They will get an invite in their inbox.'})
-      })
+    sendInvite ({Meteor, sweetalert, Collections}, inviteId) {
+      const invite = Collections.Invites.findOne(inviteId)
+
+      const sendInvite = () => {
+        Meteor.call('admin/user/invite', inviteId, (err) => {
+          if (err) {
+            return sweetalert({title: 'Error inviting', text: err.reason})
+          }
+          return sweetalert({title: 'Invited', text: 'They will get an invite in their inbox.'})
+        })
+      }
+
+      if (invite.status === 'sent') {
+        sweetalert({
+          title: 'Sure?',
+          text: 'Sending already sent invite can result in duplicate invites',
+          type: 'warning',
+          showCancelButton: true,
+          closeOnConfirm: false
+        }, sendInvite)
+      } else {
+        sendInvite()
+      }
     },
 
     removeInvite ({ Meteor, sweetalert }, inviteId) {
