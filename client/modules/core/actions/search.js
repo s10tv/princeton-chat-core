b7b6ba90cf0d18043cc2c7750c5278a7b1b76a3d
Qiming Fang <qf26@cornell.edu>
2016-02-19 14:25:09 -0800

    add some basic styles

--- client/modules/core/actions/search.js
+++ client/modules/core/actions/search.js
@@ -1,22 +1,6 @@
 
 export default {
-  search ({Meteor, LocalState}, searchTerm, searchType) {
-    let method
-    switch (searchType) {
-      case 'users':
-        method = 'search/users'
-        break
-      case 'posts': // fallthrough intentional
-      default:
-        method = 'search/posts'
-    }
-
-    Meteor.call(method, searchTerm, (err, res) => {
-      if (err) {
-        return console.log(err)
-      }
-
-      LocalState.set('SEARCH_RESULTS', res)
-    })
+  search ({FlowRouter}, searchTerm) {
+    return FlowRouter.go('search', {}, { term: searchTerm })
   }
 }
