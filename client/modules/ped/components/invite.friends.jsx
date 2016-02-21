529ab4305cf0e66d50a0230e2ced5c297ad1423a
Nurym Kudaibergen <nurim98@gmail.com>
2016-02-21 13:29:49 -0800

    status pending on pedplay fixed

--- client/modules/ped/components/invite.friends.jsx
+++ client/modules/ped/components/invite.friends.jsx
@@ -44,7 +44,7 @@ class InviteFriends extends React.Component {
               <FlatButton type='submit' style={style.button} label='Done' disabled={submitting}
                 backgroundColor={color.green} hoverColor={color.lightGreen} />
               {submitting && <LinearProgress color={color.brand.primary} />}
-              <a style={s.skip} href='/all-mine' tabIndex={-1}>Or skip for now</a>
+              <a style={s.skip} href='#' onClick={skipForNow} tabIndex={-1}>Or skip for now</a>
               <PageControl total={3} current={2} />
             </form>
           </div>
