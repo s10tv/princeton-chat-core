0871a438b61e6c4d08c78cd3cc5edaf8e600f5ec
Qiming Fang <qf26@cornell.edu>
2016-02-22 12:36:20 -0800

    move ped out

--- client/modules/ped/components/layout/index.js
+++
@@ -1,7 +0,0 @@
-import Sidebar from '../../../princeton/components/layout/sidebar.jsx'
-import Main from '../../../princeton/components/layout/main.jsx'
-import Window from '../../../princeton/components/layout/window.jsx'
-
-export default {
-  Sidebar, Window, Main,
-}
