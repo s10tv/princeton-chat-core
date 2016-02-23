f541cc4db126cf73e5aba136b704951973398520
Qiming Fang <qf26@cornell.edu>
2016-02-22 19:40:28 -0800

    princeton.chat/pedplay

--- server/methods/index.js
+++ server/methods/index.js
@@ -20,7 +20,7 @@ export default function (context) {
 
   Accounts.validateNewUser((user) => {
     if (user && user.services && user.services.facebook) {
-      throw new Meteor.Error(403, "You haven't registered yet. Register first at https://princeton.chat")
+      throw new Meteor.Error(403, "You haven't registered yet. Register first at https://pedplay.com")
     }
 
     return true
