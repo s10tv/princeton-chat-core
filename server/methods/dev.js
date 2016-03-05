c64c11670c2ea6c50e158751ce3cc74c7572277c
Qiming Fang <qf26@cornell.edu>
2016-03-04 18:55:37 -0800

    add a script to generate some fake AMA data

--- server/methods/dev.js
+++ server/methods/dev.js
@@ -1,5 +1,6 @@
 export default function ({Meteor, Collections, currentUser}) {
-  const {Notifications, Messages, Posts} = Collections
+  const {Notifications, Messages, Posts, Topics, AmaPosts,
+    AmaMessages, AmaActivities} = Collections
 
   if (process.env.ENV === 'dev') {
     Meteor.methods({
@@ -21,6 +22,67 @@ export default function ({Meteor, Collections, currentUser}) {
             status: 'active'
           })
         })
+      },
+
+      'dev/ama/create': () => {
+        const user = currentUser()
+
+        Topics.upsert({ _id: 'ama' }, {$set: {
+          displayName: 'AMA',
+          type: 'ama'
+        }})
+
+        AmaPosts.upsert({_id: 'qimingama'}, {$set: {
+          _id: 'qimingama',
+          speakerId: user._id,
+          title: 'Qiming Fang: Talk about being B0$$',
+          introText: 'Qiming is a boss. What can you say?',
+          startTime: new Date()
+        }})
+
+        AmaMessages.upsert({_id: 'qimingama-msg1'}, { $set: {
+          _id: 'qimingama-msg1',
+          ownerId: user._id,
+          amaPostId: 'qimingama',
+          content: 'What is your favorite fruit?',
+          childrenMessageIds: [
+            'qimingama-msg-reply'
+          ]
+        }})
+
+        AmaMessages.upsert({_id: 'qimingama-msg-reply'}, { $set: {
+          _id: 'qimingama-msg-reply',
+          ownerId: user._id,
+          amaPostId: 'qimingama',
+          parentMessageId: 'qimingama-msg1',
+          parentMessageOwner: user._id,
+          content: 'Banannas are my favorite fruit'
+        }})
+
+        AmaMessages.upsert({_id: 'qimingama-msg2'}, { $set: {
+          _id: 'qimingama-msg2',
+          ownerId: user._id,
+          amaPostId: 'qimingama',
+          content: 'Why is my foot hurting?'
+        }})
+
+        AmaActivities.upsert({_id: 'qimingama-msg1-activity'}, { $set: {
+          _id: 'qimingama-msg1-activity',
+          title: `${user.firstName} asked a new question`,
+          content: 'What is your favorite fruit?',
+          originatorUserId: user._id,
+          amaPostId: 'qimingama',
+          amaMessageId: 'qimingama-msg1'
+        }})
+
+        AmaActivities.upsert({_id: 'qimingama-msg1-reply-activity'}, { $set: {
+          _id: 'qimingama-msg1-reply-activity',
+          title: `${user.firstName} replied to ${user.firstName}'s question'`,
+          content: 'Banannas are my favorite fruit',
+          originatorUserId: user._id,
+          amaPostId: 'qimingama',
+          amaMessageId: 'qimingama-msg-reply'
+        }})
       }
     })
   }
