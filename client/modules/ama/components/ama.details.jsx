c19c2c3ff22a2e1b2c9b3b295c609aab1b9e39a0
Nurym Kudaibergen <nurim98@gmail.com>
2016-03-06 11:59:57 -0800

    [ama details] migrate what i have to scss

--- client/modules/ama/components/ama.details.jsx
+++ client/modules/ama/components/ama.details.jsx
@@ -2,8 +2,6 @@ import React, {PropTypes} from 'react'
 import {imageShape, userShape} from '/client/lib/shapes'
 import Radium, {StyleRoot} from 'radium'
 import {FontIcon} from '/client/lib/ui.jsx'
-import {fontSize, spacing} from '/client/configs/theme'
-import color from '/client/configs/color'
 import moment from 'moment'
 
 class AMADetails extends React.Component {
@@ -11,8 +9,8 @@ class AMADetails extends React.Component {
     console.log(this.props)
     return (
       <StyleRoot>
-        <div style={s.pageWrapper}>
-          <div style={s.main}>
+        <div className='ama-page-wrapper'>
+          <div className='ama-main'>
             <Header {...this.props}/>
           </div>
         </div>
@@ -42,7 +40,7 @@ AMADetails.propTypes = {
 
 const Header = (props) => {
   return (
-    <div style={s.headerMainDiv(data.coverUrl)}>
+    <div className='ama-header' style={{backgroundImage: `url(${data.coverUrl})`}}>
       <HeaderInnerDiv {...props}/>
       <HeaderOverlay {...props}/>
     </div>
@@ -51,22 +49,22 @@ const Header = (props) => {
 
 const HeaderInnerDiv = (props) => {
   return (
-    <div style={s.headerInnerDiv}>
-      <FontIcon className='material-icons' style={s.hamburgerButton}>menu</FontIcon>
-      <div style={s.headerAmaDetailsContainer}>
-        <div style={s.headerAmaDateContainer}>
+    <div className='ama-header-inner'>
+      <FontIcon className='material-icons' style={{fontSize: 25, color: 'white'}}>menu</FontIcon>
+      <div className='header-details-container'>
+        <div className='header-date-container'>
           <span>{moment(props.startTime).format('MMM D').toUpperCase()}</span>
           <span>{moment(props.startTime).format('ha')}</span>
         </div>
         {
           props.isLive
-          ? <div style={s.headerAmaLiveNowContainer}>
-            <div style={s.headerAmaLiveNowCircle} />
-            <span style={s.headerAmaLiveNowLabel}>LIVE NOW</span>
+          ? <div className='header-live-now-container'>
+            <div className='header-live-now-circle'/>
+            <span className='header-live-now-label'>LIVE NOW</span>
           </div>
           : null
         }
-        <span style={s.headerAmaDetailsConversationLabel}>{data.conversationLabel}</span>
+        <span className='header-conversation-label'>{data.conversationLabel}</span>
       </div>
     </div>
   )
@@ -74,108 +72,15 @@ const HeaderInnerDiv = (props) => {
 
 const HeaderOverlay = (props) => {
   return (
-    <div style={s.headerOverlayDiv}>
-      <div style={s.headerOverlayShareContainer}>
-        <span style={s.headerOverlayShareButtonsLabel}>Share</span>
-        <div style={s.headerOverlayShareButtonsRow}>
+    <div className='ama-header-overlay'>
+      <div className='share-container'>
+        <span className='share-buttons-label'>Share</span>
+        <div >
           <i className='fa fa-facebook'/>
         </div>
       </div>
     </div>
   )
-}
-
-const RIGHTBAR_WIDTH = 300
-const HEADER_HEIGHT = 240
-
-const s = {
-  pageWrapper: {
-    display: 'flex',
-    minHeight: '100vh'
-  },
-  main: {
-    display: 'flex',
-    flex: '0 0 100%'
-  },
-  headerMainDiv: (url) => ({
-    display: 'flex',
-    flexShrink: 0,
-    flexBasis: '100%',
-    height: HEADER_HEIGHT,
-    backgroundImage: `url(${url})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat'
-  }),
-  headerInnerDiv: {
-    display: 'flex',
-    flexGrow: '1',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    padding: spacing.x3
-  },
-  headerAmaDetailsContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    bottom: spacing.x3,
-    left: spacing.x3
-  },
-  headerAmaDateContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexShrink: '0',
-    padding: spacing.x1,
-    color: 'white',
-    borderRadius: 5,
-    border: '1px solid white',
-    backgroundColor: color.translucentBlack,
-    marginRight: spacing.x4
-  },
-  headerAmaLiveNowContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    marginRight: spacing.x4
-  },
-  headerAmaLiveNowCircle: {
-    borderRadius: '50%',
-    width: 10,
-    height: 10,
-    backgroundColor: color.dullRed,
-    border: `1px solid ${color.dullRed}`,
-    marginRight: spacing.x1
-  },
-  headerAmaLiveNowLabel: {
-    color: 'white',
-    fontWeight: 300
-  },
-  headerAmaDetailsConversationLabel: {
-    fontSize: fontSize.h4,
-    marginBottom: spacing.x1,
-    color: 'white'
-  },
-  headerOverlayDiv: {
-    display: 'flex',
-    flex: `0 0 ${RIGHTBAR_WIDTH}px`,
-    backgroundColor: color.translucentBlack
-  },
-  headerOverlayShareContainer: {
-    padding: spacing.x4
-  },
-  headerOverlayShareButtonsRow: {
-    display: 'flex',
-    justifyContent: 'space-around'
-  },
-  headerOverlayShareButtonsLabel: {
-    color: 'white',
-    fontSize: fontSize.h6
-  },
-  hamburgerButton: {
-    fontSize: 25,
-    color: 'white'
-  }
 }
 
 const data = {
