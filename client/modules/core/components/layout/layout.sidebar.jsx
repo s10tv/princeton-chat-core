bfdba6f1609c9da4577658fc274ce054d30b63f1
Tony Xiao <tonyx.ca@gmail.com>
2016-03-05 16:19:14 -0800

    Switch sidebar to use scss rather than inline style

--- client/modules/core/components/layout/layout.sidebar.jsx
+++ client/modules/core/components/layout/layout.sidebar.jsx
@@ -1,70 +1,11 @@
 import React from 'react'
-import Radium, {Style} from 'radium'
-import RaisedButton from '../../../../../node_modules/material-ui/lib/raised-button'
-import FontIcon from '../../../../../node_modules/material-ui/lib/font-icon'
+import RaisedButton from 'material-ui/lib/raised-button'
+import FontIcon from 'material-ui/lib/font-icon'
 import {i18n} from '/client/configs/env'
-import {color, fontSize, spacing} from '/client/configs/theme'
 import SidebarHeader from './sidebarHeader.jsx'
 const theme = i18n('secondaryMuiTheme')
 
-const s = {
-  newPost: {
-    alignSelf: 'center',
-    margin: spacing.x2
-  },
-  newPostIcon: {
-    verticalAlign: 'middle',
-    height: '100%',
-    marginLeft: 8
-  },
-  sidebar: {
-    transition: '0.5s ease',
-    width: 0,
-    overflow: 'hidden',
-    '@media (min-width: 768px)': {
-      width: 240
-    }
-  },
-  sidebarRules: {
-    '': {
-      display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: color.princeton.black,
-      color: color.white
-    },
-    h3: {
-      fontSize: fontSize.sm,
-      color: color.gray,
-      fontWeight: 'normal',
-      paddingLeft: spacing.x2,
-      paddingBottom: 4
-    },
-    ul: {
-      padding: 0
-    },
-    li: {
-      listStyle: 'none',
-      marginRight: spacing.x3
-    },
-    a: {
-      paddingLeft: spacing.x3,
-      display: 'block',
-      width: '100%',
-      color: color.white,
-      borderRadius: '0 3px 3px 0'
-    },
-    'a:hover': {
-      backgroundColor: color.gray,
-      opacity: 1
-    },
-    'ul.medium a': {
-      paddingTop: 4,
-      paddingBottom: 4
-    }
-  }
-}
-
-class Sidebar extends React.Component {
+export default class Sidebar extends React.Component {
   getChildContext () {
     return {
       muiTheme: theme
@@ -73,14 +14,13 @@ class Sidebar extends React.Component {
   render () {
     const {user, onLogout, onTapSettings, isFullAdmin} = this.props
     return (
-      <nav className='sidebar' style={s.sidebar}>
-        <Style scopeSelector='.sidebar' rules={s.sidebarRules} />
+      <nav className='sidebar'>
         <SidebarHeader
           user={user}
           onLogout={onLogout}
           onTapSettings={onTapSettings} />
-        <RaisedButton primary linkButton label='New Post' labelPosition='after' href='/add-post' style={s.newPost}>
-          <FontIcon className='material-icons' color='white' style={s.newPostIcon}>
+        <RaisedButton primary linkButton label='New Post' labelPosition='after' href='/add-post' className='new-post'>
+          <FontIcon className='material-icons new-post-icon' color='white'>
              add_circle
           </FontIcon>
         </RaisedButton>
@@ -91,7 +31,7 @@ class Sidebar extends React.Component {
           <li><a href='/choose-topics'>Explore Channels</a></li>
         </ul>
         <h3>CHANNELS</h3>
-        <ul style={s.ul}>
+        <ul>
           {this.props.followedTopics.map((topic) =>
             <li key={topic._id}>
               <a href={`/topics/${topic._id}`}>{`# ${topic.displayName}`}</a>
@@ -113,5 +53,3 @@ Sidebar.propTypes = {
   onTapSettings: React.PropTypes.func.isRequired,
   isFullAdmin: React.PropTypes.bool.isRequired
 }
-
-export default Radium(Sidebar)
