f11ac58cc79312132cdaeac6bf33e46211afe527
Tony Xiao <tonyx.ca@gmail.com>
2016-03-08 01:36:14 -0800

    media query the easy way with scss mixin

--- client/modules/core/containers/layout.js
+++ client/modules/core/containers/layout.js
@@ -1,4 +1,4 @@
-import {useDeps, composeWithTracker, composeAll} from 'mantra-core'
+import {useDeps, composeAll} from 'mantra-core'
 import {connect} from 'react-redux'
 import Layout from '/client/modules/core/components/layout/layout.jsx'
 
@@ -6,12 +6,6 @@ const mapStateToProps = (state) => ({
   sidebarOpen: state.core.sidebar.open,
   sidebarDocked: state.browser.greaterThan.small
 })
-
-export const composer = ({Meteor}, onData) => {
-  if (Meteor.subscribe('userData').ready()) {
-    onData(null, {})
-  }
-}
 
 const depsMapper = (context, actions) => ({
   toggleSidebar: actions.sidebar.toggle,
@@ -22,6 +16,5 @@ const depsMapper = (context, actions) => ({
 
 export default composeAll(
   connect(mapStateToProps),
-  composeWithTracker(composer),
   useDeps(depsMapper)
 )(Layout)
