e3e58c99d02bf0695db7e6ba3b66adf221e26aec
Nurym Kudaibergen <nurim98@gmail.com>
2016-03-10 10:59:55 -0800

    fix hairline letter avatar font-weights on safari

--- client/lib/helpers.jsx
+++ client/lib/helpers.jsx
@@ -58,14 +58,14 @@ export const MediumListItem = (props) => (
 export const SquareAvatar = (props) => (
   <CoverAvatar {...props} size={props.length} style={Object.assign({
     borderRadius: 5,
-    fontWeight: 200
+    fontWeight: 300
   }, props.style)}>
     {props.children}
   </CoverAvatar>
 )
 
 export const LetterAvatar = (props) => (
-  <Avatar {...props} style={Object.assign({}, {fontWeight: 200}, props.style)}>
+  <Avatar {...props} style={Object.assign({}, {fontWeight: 300}, props.style)}>
     {props.children}
   </Avatar>
 )
