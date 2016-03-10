d140348cf1ad30c1126a58d1cb6029f496dc914e
Tony Xiao <tonyx.ca@gmail.com>
2016-03-10 12:42:00 -0800

    Move all the server-side dochead into react-helmet to manage

--- server/dochead/docheadRoutes.jsx
+++ server/dochead/docheadRoutes.jsx
@@ -2,23 +2,37 @@ import React from 'react'
 import {IndexRoute, Route} from 'react-router'
 import Helmet from 'react-helmet'
 
-const wrap = (Component) => (
-  (location, cb) => {
-    cb(null, Component)
-  }
+const wrap = (Component) => (location, cb) => {
+  cb(null, Component)
+}
+
+const makeHelmet = (overrideProps) => () => (
+  <Helmet
+    title='Princeton.Chat'
+    meta={[
+      {property: 'fb:app_id', content: '942109702548809'},
+      {property: 'og:url', content: 'https://princeton.chat'},
+      {property: 'og:title', content: 'Princeton.Chat'},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:description', content: 'A private community that connects Princetonians based on shared interests and common needs.'},
+      {property: 'og:image', content: 'https://s10tv.blob.core.windows.net/s10tv-prod/princetonchat.jpg'},
+      {name: 'description', content: 'A private community that connects Princetonians based on shared interests and common needs.'}
+    ]} {...overrideProps} />
 )
 
 export default function ({Collections}) {
   return (
     <Route path='/'>
-      <IndexRoute component={() => <Helmet title='Princeton.Chat' />} />
-      <Route path='tonyx' component={() => <Helmet title='Tony Xiao' />} />
-      <Route path='poshak' component={() => <Helmet title='Poshak Agrawal' />} />
+      <IndexRoute component={makeHelmet()} />
+      <Route path='tonyx' component={() => makeHelmet({title: 'Tony Xiao'})} />
+      <Route path='poshak' component={() => makeHelmet({title: 'Poshak Agrawal'})} />
       <Route path='channels/:channelId' getComponent={wrap(({params}) => {
         const channel = Collections.Topics.findOne(params.channelId)
-        return channel ? (<Helmet title={channel.displayName} />) : <noscript />
-        })} />
-      <Route path='*' component={() => <Helmet title='Princeton.Chat | Not Found' />} />
+        return channel ? makeHelmet({
+          title: channel.displayName
+        })() : <noscript />
+      })} />
+      <Route path='*' component={makeHelmet()} />
     </Route>
   )
 }
--- server/dochead/injectDochead.js
+++ server/dochead/injectDochead.js
@@ -9,7 +9,7 @@ import Helmet from 'react-helmet'
 // meteor algorithm to check if this is a meteor serving http request or not
 const IsAppUrl = (url) => {
   if (url === '/favicon.ico' || url === '/robots.txt') {
-    return false;
+    return false
   }
   // NOTE: app.manifest is not a web standard like favicon.ico and
   // robots.txt. It is a file name we have chosen to use for HTML5
@@ -18,20 +18,20 @@ const IsAppUrl = (url) => {
   // once we have server side routing, this won't be needed as
   // unknown URLs with return a 404 automatically.
   if (url === '/app.manifest') {
-    return false;
+    return false
   }
   // Avoid serving app HTML for declared routes such as /sockjs/.
   if (RoutePolicy.classify(url)) {
-    return false;
+    return false
   }
-  return true;
+  return true
 }
 
 const patchResWrite = (originalWrite, head) => {
-  return function(data) {
+  return function (data) {
     if (typeof data === 'string' && data.indexOf('<!DOCTYPE html>') === 0) {
       // Add react-helmet stuff in the header (yay SEO!)
-      data = data.replace('<head>', 
+      data = data.replace('<head>',
         `<head>${head.title}${head.base}${head.meta}${head.link}${head.script}`)
       // '<head>' + head.title + head.base + head.meta + head.link + head.script
     }
@@ -39,24 +39,21 @@ const patchResWrite = (originalWrite, head) => {
   }
 }
 
-export default function(routes) {
-  Meteor.bindEnvironment(function () {
-    console.log('Will inject dochead handler into Meteor')
-    WebApp.connectHandlers.use(Meteor.bindEnvironment(function (req, res, next) {
-      if (!IsAppUrl(req.url)) {
-        return next()
+export default Meteor.bindEnvironment((routes) => {
+  console.log('Will inject dochead handler into Meteor')
+  WebApp.connectHandlers.use(Meteor.bindEnvironment((req, res, next) => {
+    if (!IsAppUrl(req.url)) {
+      return next()
+    }
+
+    match({routes, location: req.url}, Meteor.bindEnvironment((err, redirectLocation, renderProps) => {
+      if (!err && !redirectLocation && renderProps) {
+        console.log('Will patch res.write with dochead info url =', req.url)
+        ReactDOMServer.renderToStaticMarkup(React.createElement(RouterContext, renderProps))
+        const head = Helmet.rewind()
+        res.write = patchResWrite(res.write, head)
       }
-      
-      match({routes, location: req.url}, Meteor.bindEnvironment((err, redirectLocation, renderProps) => {
-        if (!err && !redirectLocation && renderProps) {
-          console.log('Will patch res.write with dochead info url =', req.url)
-          ReactDOMServer.renderToStaticMarkup(React.createElement(RouterContext, renderProps))
-          const head = Helmet.rewind()
-          res.write = patchResWrite(res.write, head)
-        }
-        next()
-      }))
-      
+      next()
     }))
-  })()
-}
+  }))
+})
