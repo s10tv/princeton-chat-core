80f0caf5a90123da90fd616fd5ac40637421b239
Nurym Kudaibergen <nurim98@gmail.com>
2016-04-13 02:44:29 -0700

    Update settings.js

--- server/configs/settings.js
+++ server/configs/settings.js
@@ -7,8 +7,8 @@ export default function ({ ServiceConfiguration, Meteor }) {
 
   ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: process.env.FB_APP_ID || '1687343324838305',
-    secret: process.env.FB_APP_SECRET || '8bc99973abd08ad512642ea8c84d1bdb'
+    appId: process.env.FB_APP_ID || '',
+    secret: process.env.FB_APP_SECRET || ''
   })
 
   Object.assign(Meteor.settings.public, {
@@ -17,12 +17,12 @@ export default function ({ ServiceConfiguration, Meteor }) {
     enableBrowserSync: process.env.BROWSER_SYNC || false,
     enableReduxLogger: false,
     rootUrl: stripTrailingSlash(process.env.ROOT_URL),
-    audience: process.env.AUDIENCE || 'princeton',
+    audience: process.env.AUDIENCE || '',
     environment: process.env.ENV || 'dev',
-    amplitudeKey: process.env.AMPLITUDE_KEY || 'bc1101820f7bda64561e70be2594befd',
-    filestackKey: process.env.FILESTACK_KEY || 'AdtrkZqvsS8qdeVHCcYlbz',
-    googleAnalyticsKey: process.env.GOOGLE_ANALYTICS_KEY || 'UA-73874104-1',
-    mailgunPublicKey: process.env.MAILGUN_PUBLIC_API_KEY || 'pubkey-4876fe247939932752b0e3d3838234aa',
-    unsplashKey: process.env.UNSPLASH_KEY || '3b28c5415d07e18260e66e9d5bc3f2434c2aa64bed071d52e8c62c3da800612e'
+    amplitudeKey: process.env.AMPLITUDE_KEY || '',
+    filestackKey: process.env.FILESTACK_KEY || '',
+    googleAnalyticsKey: process.env.GOOGLE_ANALYTICS_KEY || 'UA-XXXXXXXX-X',
+    mailgunPublicKey: process.env.MAILGUN_PUBLIC_API_KEY || '',
+    unsplashKey: process.env.UNSPLASH_KEY || ''
   })
 }
